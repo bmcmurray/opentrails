328712cffc9e83b1a372e22d64170e93a1c80341
g1adrift@gmail.com <g1adrift@gmail.com@9f6135f8-51fe-11de-936b-e9512c4d2277>
2009-05-21 13:09:10 +0000

    import of skeleton

---
+++ trunk/src/net/cascadingstyle/android/lTrax/L_Trax.java
@@ -0,0 +1,13 @@
+package net.cascadingstyle.android.lTrax;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class L_Trax extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
