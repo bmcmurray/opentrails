62f529e81ba09d863b90af6a1ee343db825a61d1
g1adrift@gmail.com <g1adrift@gmail.com@9f6135f8-51fe-11de-936b-e9512c4d2277>
2009-05-21 14:54:21 +0000

    attempt at location services

--- trunk/src/net/cascadingstyle/android/lTrax/L_Trax.java
+++ trunk/src/net/cascadingstyle/android/lTrax/L_Trax.java
@@ -1,6 +1,13 @@
 package net.cascadingstyle.android.lTrax;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -8,12 +15,20 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class L_Trax extends Activity {
+	private LocationManager lm;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         initButtons();
+        
+    	lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    	List<String> providers = lm.getAllProviders();
+		Toast.makeText(getApplicationContext(), 
+				providers.toString(), 
+				Toast.LENGTH_LONG).show();
     }
     
     private void initButtons(){
@@ -26,9 +41,23 @@ public class L_Trax extends Activity {
     }
     
     public void favorite(){
+    	String providerString = lm.getBestProvider(new Criteria(), true);
+    	if (providerString == null){
+    		Toast.makeText(getApplicationContext(), 
+    				getString(R.string.error_no_providers), 
+    				Toast.LENGTH_LONG).show();
+    	}
+    	LocationProvider provider = lm.getProvider(providerString);
     	
+    	Location l = lm.getLastKnownLocation(providerString);
+    	if (l != null){
 		Toast.makeText(getApplicationContext(), 
-				getString(R.string.favorite_result), 
-				Toast.LENGTH_LONG).show();
+				getString(R.string.favorite_result) + ": " + l.toString(), 
+				Toast.LENGTH_SHORT).show();
+    	}else{
+    		Toast.makeText(getApplicationContext(), 
+    				getString(R.string.error_no_fix), 
+    				Toast.LENGTH_LONG).show();
+    	}
     }
 }
---
+++ trunk/src/net/cascadingstyle/android/lTrax/TrackerService.java
@@ -0,0 +1,15 @@
+package net.cascadingstyle.android.lTrax;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TrackerService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
