e4d5787d3ff624a1977e568b4988cfa2f4f71af2
g1adrift@gmail.com <g1adrift@gmail.com@9f6135f8-51fe-11de-936b-e9512c4d2277>
2009-05-22 17:59:18 +0000

    moved location manager to service

--- trunk/src/net/cascadingstyle/android/lTrax/L_Trax.java
+++ trunk/src/net/cascadingstyle/android/lTrax/L_Trax.java
@@ -1,21 +1,17 @@
 package net.cascadingstyle.android.lTrax;
 
-import java.util.List;
-
 import android.app.Activity;
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
-import android.location.LocationProvider;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class L_Trax extends Activity {
-	private LocationManager lm;
+public class L_Trax extends Activity implements ServiceConnection {
+	
 	
     /** Called when the activity is first created. */
     @Override
@@ -24,11 +20,7 @@ public class L_Trax extends Activity {
         setContentView(R.layout.main);
         initButtons();
         
-    	lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-    	List<String> providers = lm.getAllProviders();
-		Toast.makeText(getApplicationContext(), 
-				providers.toString(), 
-				Toast.LENGTH_LONG).show();
+        bindService(new Intent(this, TrackerService.class), this, BIND_AUTO_CREATE);
     }
     
     private void initButtons(){
@@ -41,23 +33,16 @@ public class L_Trax extends Activity {
     }
     
     public void favorite(){
-    	String providerString = lm.getBestProvider(new Criteria(), true);
-    	if (providerString == null){
-    		Toast.makeText(getApplicationContext(), 
-    				getString(R.string.error_no_providers), 
-    				Toast.LENGTH_LONG).show();
-    	}
-    	LocationProvider provider = lm.getProvider(providerString);
     	
-    	Location l = lm.getLastKnownLocation(providerString);
-    	if (l != null){
-		Toast.makeText(getApplicationContext(), 
-				getString(R.string.favorite_result) + ": " + l.toString(), 
-				Toast.LENGTH_SHORT).show();
-    	}else{
-    		Toast.makeText(getApplicationContext(), 
-    				getString(R.string.error_no_fix), 
-    				Toast.LENGTH_LONG).show();
-    	}
     }
+
+	public void onServiceConnected(ComponentName name, IBinder service) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onServiceDisconnected(ComponentName name) {
+		// TODO Auto-generated method stub
+		
+	}
 }
