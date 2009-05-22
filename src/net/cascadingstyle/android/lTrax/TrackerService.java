d9b3d01c7457a6fd339c9bdad5dba1a4f987c954
g1adrift@gmail.com <g1adrift@gmail.com@9f6135f8-51fe-11de-936b-e9512c4d2277>
2009-05-22 17:59:18 +0000

    moved location manager to service

--- src/net/cascadingstyle/android/lTrax/TrackerService.java
+++ src/net/cascadingstyle/android/lTrax/TrackerService.java
@@ -1,10 +1,25 @@
 package net.cascadingstyle.android.lTrax;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
+import java.util.List;
 
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * A location tracking service. Can record tracklogs as well as individual waypoints.
+ * 
+ * @author steve
+ *
+ */
 public class TrackerService extends Service {
+	private LocationManager lm;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -12,4 +27,38 @@ public class TrackerService extends Service {
 		return null;
 	}
 
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+    	lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    	List<String> providers = lm.getAllProviders();
+		Toast.makeText(getApplicationContext(), 
+				providers.toString(), 
+				Toast.LENGTH_LONG).show();
+	}
+	
+	
+	
+	public void saveWaypoint(){
+		String providerString = lm.getBestProvider(new Criteria(), true);
+    	if (providerString == null){
+    		Toast.makeText(getApplicationContext(), 
+    				getString(R.string.error_no_providers), 
+    				Toast.LENGTH_LONG).show();
+    	}
+
+    	LocationProvider provider = lm.getProvider(providerString);
+    	
+    	Location l = lm.getLastKnownLocation(providerString);
+    	if (l != null){
+		Toast.makeText(getApplicationContext(), 
+				getString(R.string.favorite_result) + ": " + l.toString(), 
+				Toast.LENGTH_SHORT).show();
+    	}else{
+    		Toast.makeText(getApplicationContext(), 
+    				getString(R.string.error_no_fix), 
+    				Toast.LENGTH_LONG).show();
+    	}
+	}
 }
