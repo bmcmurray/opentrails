02e14fb8f049f0fd2a6d4d06755ea4cd9a66934d
briancmcmurray <briancmcmurray@9f6135f8-51fe-11de-936b-e9512c4d2277>
2009-05-28 14:48:33 +0000

    NOT WORKING. Committing the basics of a DbAdapter ported from one of the Android tutorials. Still needs more refactoring to actually work with our database design; and testing of loading the custom .sql file instead of having the create string in the class.

---
+++ trunk/res/raw/ltrax_sql.sql
@@ -0,0 +1,31 @@
+CREATE TABLE 'Track'
+(
+'id' INTEGER PRIMARY KEY,
+'title' VARCHAR(255),
+'created' TIMESTAMP DEFAULT '(DATETIME(\'now\'))' NOT NULL,
+'modified' TIMESTAMP
+);
+
+
+
+CREATE TABLE 'TrackPoint'
+(
+'id' INTEGER PRIMARY KEY UNIQUE ,
+'tid' INTEGER NOT NULL,
+'lon' FLOAT NOT NULL,
+'lat' FLOAT NOT NULL,
+'timestamp' TIMESTAMP DEFAULT '(DATETIME(\'now\'))' NOT NULL
+);
+
+
+
+CREATE TABLE 'Point'
+(
+'id' INTEGER PRIMARY KEY UNIQUE ,
+'title' VARCHAR(255),
+'lon' FLOAT NOT NULL,
+'lat' FLOAT NOT NULL,
+'timestamp' TIMESTAMP DEFAULT '(DATETIME(\'now\'))' NOT NULL
+);
+
+
---
+++ trunk/src/net/cascadingstyle/android/lTrax/LocationDbAdapter.java
@@ -0,0 +1,198 @@
+/**
+ * 
+ */
+package net.cascadingstyle.android.lTrax;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.content.res.Resources;
+
+/**
+ * @author bmcmurray
+ * 
+ * Simple location database access helper class. Defines the basic CRUD operations
+ * for the notepad example, and gives the ability to list all notes as well as
+ * retrieve or modify a specific note.
+ * 
+ * Adapted from NotesDbAdapter.java from http://developer.android.com/guide/tutorials/notepad/notepad-ex1.html
+ * 
+ */
+public class LocationDbAdapter {
+	
+	public static final String KEY_TITLE = "title";
+    public static final String KEY_BODY = "body";
+    public static final String KEY_ROWID = "_id";
+
+    private static final String TAG = "LTraxLocationDbAdapter";
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+    
+    /**
+     * Database creation sql statement
+     */
+    private static Resources r;
+    private static InputStream is = r.openRawResource(R.raw.ltrax_sql);
+    
+    private static String loadSqlFile(InputStream is) {
+    	StringBuilder sqlString = new StringBuilder();
+    	
+    	try {
+    		for (BufferedReader sqlReader = new BufferedReader(new InputStreamReader(is), 16000); sqlReader.ready();) {
+    			sqlString.append(sqlReader.readLine());
+    		}
+    		
+    		return sqlString.toString();
+    	} catch (Exception e) {
+    		Log.w(TAG, "Error trying to read the sql file.");
+    		return "";
+    	}
+    }
+    
+    private static final String DATABASE_CREATE = loadSqlFile(is);
+
+    private static final String DATABASE_NAME = "ltrax_data";
+    private static final String DATABASE_TABLE = "notes";
+    private static final int DATABASE_VERSION = 1;
+
+    private final Context mCtx;
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS notes");
+            onCreate(db);
+        }
+    }
+
+    /**
+     * Constructor - takes the context to allow the database to be
+     * opened/created
+     * 
+     * @param ctx the Context within which to work
+     */
+    public LocationDbAdapter(Context ctx) {
+        this.mCtx = ctx;
+    }
+
+    /**
+     * Open the notes database. If it cannot be opened, try to create a new
+     * instance of the database. If it cannot be created, throw an exception to
+     * signal the failure
+     * 
+     * @return this (self reference, allowing this to be chained in an
+     *         initialization call)
+     * @throws SQLException if the database could be neither opened or created
+     */
+    public LocationDbAdapter open() throws SQLException {
+        mDbHelper = new DatabaseHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+    
+    public void close() {
+        mDbHelper.close();
+    }
+
+
+    /**
+     * Create a new note using the title and body provided. If the note is
+     * successfully created return the new rowId for that note, otherwise return
+     * a -1 to indicate failure.
+     * 
+     * @param title the title of the note
+     * @param body the body of the note
+     * @return rowId or -1 if failed
+     */
+    public long createNote(String title, String body) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_TITLE, title);
+        initialValues.put(KEY_BODY, body);
+
+        return mDb.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+    /**
+     * Delete the note with the given rowId
+     * 
+     * @param rowId id of note to delete
+     * @return true if deleted, false otherwise
+     */
+    public boolean deleteNote(long rowId) {
+
+        return mDb.delete(DATABASE_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+    /**
+     * Return a Cursor over the list of all notes in the database
+     * 
+     * @return Cursor over all notes
+     */
+    public Cursor fetchAllNotes() {
+
+        return mDb.query(DATABASE_TABLE, new String[] {KEY_ROWID, KEY_TITLE,
+                KEY_BODY}, null, null, null, null, null);
+    }
+
+    /**
+     * Return a Cursor positioned at the note that matches the given rowId
+     * 
+     * @param rowId id of note to retrieve
+     * @return Cursor positioned to matching note, if found
+     * @throws SQLException if note could not be found/retrieved
+     */
+    public Cursor fetchNote(long rowId) throws SQLException {
+
+        Cursor mCursor =
+
+                mDb.query(true, DATABASE_TABLE, new String[] {KEY_ROWID,
+                        KEY_TITLE, KEY_BODY}, KEY_ROWID + "=" + rowId, null,
+                        null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+
+    }
+
+    /**
+     * Update the note using the details provided. The note to be updated is
+     * specified using the rowId, and it is altered to use the title and body
+     * values passed in
+     * 
+     * @param rowId id of note to update
+     * @param title value to set note title to
+     * @param body value to set note body to
+     * @return true if the note was successfully updated, false otherwise
+     */
+    public boolean updateNote(long rowId, String title, String body) {
+        ContentValues args = new ContentValues();
+        args.put(KEY_TITLE, title);
+        args.put(KEY_BODY, body);
+
+        return mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }	
+}
