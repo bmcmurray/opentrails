794129b3e9e28b77945b869d66a6ab4c421d841f
briancmcmurray <briancmcmurray@9f6135f8-51fe-11de-936b-e9512c4d2277>
2009-06-02 14:11:36 +0000

    installs the database

--- trunk/src/net/cascadingstyle/android/lTrax/L_Trax.java
+++ trunk/src/net/cascadingstyle/android/lTrax/L_Trax.java
@@ -6,18 +6,22 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class L_Trax extends Activity implements ServiceConnection {
-	
+	private static final String TAG = "LTraxMain";
+	private LocationDbAdapter mDbHelper;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mDbHelper = new LocationDbAdapter(this);
+        mDbHelper.open();
         initButtons();
         
         bindService(new Intent(this, TrackerService.class), this, BIND_AUTO_CREATE);
